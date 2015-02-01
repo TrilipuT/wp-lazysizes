0d53ae54c1d03f035dafd712df3f6d4c70790121
Vitaly Nikolaev <vitaly@pingbull.no>
2015-02-01 10:18:51 +0200

    addClass method added

--- wp-lazysizes.php
+++ wp-lazysizes.php
@@ -105,7 +105,7 @@ if ( ! class_exists( 'LazySizes' ) ) :
 
         public function filter_iframes($html)
         {
-            return str_replace(' src=', ' class="lazyload" data-src=', $html);
+            return $this->_addClass($html,'lazyload');
         }
 
         function filter_images( $content, $type = 'ratio' ) {
@@ -175,6 +175,26 @@ if ( ! class_exists( 'LazySizes' ) ) :
 
         function get_url( $path = '' ) {
             return plugins_url( ltrim( $path, '/' ), __FILE__ );
+        }
+
+        private function _addClass($htmlString = '', $newClass) {
+            $pattern = '/class="([^"]*)"/';
+
+            // class attribute set
+            if (preg_match($pattern, $htmlString, $matches)) {
+                $definedClasses = explode(' ', $matches[1]);
+                if (!in_array($newClass, $definedClasses)) {
+                    $definedClasses[] = $newClass;
+                    $htmlString = str_replace($matches[0], sprintf('class="%s"', implode(' ', $definedClasses)), $htmlString);
+                }
+            }
+
+            // class attribute not set
+            else {
+                $htmlString = preg_replace('/(\<.+\s)/', sprintf('$1class="%s" ', $newClass), $htmlString);
+            }
+
+            return $htmlString;
         }
     }
 
